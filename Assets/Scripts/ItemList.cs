7def1c622af5f0e0ecdf95e8d3df3f7bf2e59fdf
akiirohappa <kihara.k2000@gmail.com>
2019-05-22 13:33:53 +0900

    ショップ少し

--- Assets/Scripts/ItemList.cs
+++ Assets/Scripts/ItemList.cs
@@ -8,15 +8,28 @@ public enum ItemTag
 }
 public class ItemList : MonoBehaviour
 {
+    [SerializeField]List<ItemData> myItem;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myItem = new List<ItemData>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    List<ItemData> GetItemList()
+    {
+        return myItem;
+    }
+    void SetItemList(List<ItemData> it)
+    {
+        myItem = it;
+    }
+    void SetItemList(ItemData it)
+    {
+        myItem.Add(it);
     }
 }
